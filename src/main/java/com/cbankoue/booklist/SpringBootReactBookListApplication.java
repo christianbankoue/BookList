0e4127bb7e1c76567940f536d5431bb449c0458a
Christian Bankoue <seniorbankoue@yahoo.ca>
2021-11-01 19:40:52 +0200

    initial commit
    Book List Spring and React

---
+++ src/main/java/com/cbankoue/booklist/SpringBootReactBookListApplication.java
@@ -0,0 +1,13 @@
+package com.cbankoue.booklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactBookListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactBookListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbankoue/booklist/SpringBootReactBookListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbankoue.booklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactBookListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
